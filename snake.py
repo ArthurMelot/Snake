465cca99866a838345baa97ab3e86567404d043a
ArthurMelot <arthur.melot@gmail.com>
2023-09-06 15:19:00 +0200

    Colors instead of images

--- snake.py
+++ snake.py
@@ -9,26 +9,11 @@ up_border = 100
 border = 16
 width = 2*border+tile_size*ncol
 height = up_border + tile_size*nrow
-window = pygame.display.set_mode(width,height)
+window = pygame.display.set_mode((width,height))
 
-#Images
-empty = pygame.image.load("Images/empty.png")
-fruit = pygame.image.load("Images/fruit.png")
-
-head_up = pygame.image.load("Images/head_up.png")
-head_right = pygame.transform.rotate(head_up,90)
-head_down = pygame.transform.rotate(head_up,180)
-head_left = pygame.transform.rotate(head_up,270)
-
-body_vert = pygame.image.load("Images/body_vertical.png")
-body_horiz = pygame.transform.rotate(body_vert,90)
-
-tail_up = pygame.image.load("Images/tail_up.png")
-tail_right = pygame.transform.rotate(tail_up,270)
-tail_down = pygame.transform.rotate(tail_up,180)
-tail_left = pygame.transform.rotate(tail_up,90)
-
-turn_UR = pygame.image.load("Images/turn_UR.png")
-turn_UL = pygame.transform.rotate(turn_UR,90)
-turn_DL = pygame.transform.rotate(turn_UR,180)
-turn_DR = pygame.transform.rotate(turn_UR,270)
+#Colors
+black = pygame.Color(0,0,0)
+white = pygame.Color(255,255,255)
+red = pygame.Color(255,0,0)
+green = pygame.Color(0,255,0)
+blue = pygame.Color(0,0,255)
